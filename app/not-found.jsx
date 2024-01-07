4558840470f769d4f58d03055a326992a898f815
Amaan Khan <amaankhan@Amaans-MacBook-Pro.local>
2024-01-07 17:46:16 +0530

    feat: adds 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Link from "next/link";
 
 const NotFoundPage = () => {
 	return (
-		<div>
-			Page Not Found.
-		</div>
+		<section className='max-w-[1200px] mx-auto my-20'>
+			<div className='text-center'>
+				<h2 className='mb-4'>Oops! The page can’t be found.</h2>
+				<p className='mb-2'>It looks like nothing was found at this location.</p>
+				<p className='text-sm'>
+					<Link href='/' className='text-[#E46D62]'>Go Back to Home</Link>
+				</p>
+			</div>
+		</section>
 	);
 };
 
